4dd0ee33d854a876502de33f62e260669440ccd4
NOTHINGx555 <170884425+NOTHINGx555@users.noreply.github.com>
2024-12-06 18:13:35 +0100

    Update .lua

--- .lua
+++ .lua
@@ -1,3 +1,3 @@
-print "start"
-loadstring(game:HttpGet("https://gist.githubusercontent.com/NOTHINGx555/091eafbbee61c65999e336d3b1315d15/raw/8b0a5ae0002c3ad908a0c097464f86d84438723a/NOTHING.lua"))()
-print "end"
+print "start load script"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NOTHINGx555/load/refs/heads/main/.lua"))()
+print "end load script"
